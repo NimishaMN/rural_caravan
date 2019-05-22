ef6cd22b015b0d868de043d36b55de7f355d3730
Vishwajit Kolekar <vishwajit@profitbooks.net>
2019-05-22 11:32:02 +0530

    new changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,7 +15,7 @@ class DashboardController < ApplicationController
   	p @payables
 
 
-    @male_employees = ActivityDetail.select("employees").where(created_at: '2019-04-05')
+    @male_employees = ActivityDetail.select("employes").where(created_at: '2019-04-05')
     @female_employees = Employee.where(gender: 'female')
     p @male_employees
     p @female_employees
