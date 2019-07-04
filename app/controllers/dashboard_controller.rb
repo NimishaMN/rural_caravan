7f84806ce9e701a6a98bbca6776bcd09d5c200d9
NimishaMN <nimisha.mn@gmail.com>
2019-07-04 20:58:09 +0530

    dashboard reverts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,14 +9,9 @@ class DashboardController < ApplicationController
 
   	@month = Date::MONTHNAMES
     # @months = Date.new(:created_at)
-  	# @receivables = Income.all.group_by{ |i| i.created_at.beginning_of_month } #(:created_at, :amount).count
-    # @receivables = Income.select("date_trunc('month', created_at) as month").group("month", :amount)
-    @receivables = Income.group_by_month(:created_at).count
+  	@receivables = Income.group(:created_at, :amount).count
   	p @receivables
-  	# @payables = Expense.all.group_by{ |i| i.created_at.beginning_of_month } #.group(:created_at, :amount).count
-    # @payables = Expense.select("date_trunc('month', created_at) as month").group("month", :amount)
-    @payables = Expense.group_by_month(:created_at).count
-
+  	@payables = Expense.group(:created_at, :amount).count
   	p @payables
 
 
