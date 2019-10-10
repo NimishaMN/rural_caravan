9077b53616c4c1eabef627bb538a922e05751335
NimishaMN <nimisha.mn@gmail.com>
2019-10-10 23:58:32 +0530

    json fixes for instapayload

--- app/models/instapayload.rb
+++ app/models/instapayload.rb
@@ -8,16 +8,16 @@ module Instamojo
 
     def get_payload
     {
-      "purpose" : "Subscription",
-      "amount": @user.amount,
-      "buyer_name": @user.email,
-      "email": @user.email,
-      "phone": "9890060986",
-      "redirect_url": "http://www.egramvyapar.com/billings/",
-      "send_email": true,
-      "send_sms": true,
-      "webhook": "http://www.egramvyapar.com/dashboard/index",
-      "allow_repeated_payments": false
+      "purpose" => "Subscription",
+      "amount" => @user.amount,
+      "buyer_name"=> @user.email,
+      "email"=> @user.email,
+      "phone"=> "9890060986",
+      "redirect_url"=> "http://www.egramvyapar.com/billings",
+      "send_email"=> true,
+      "send_sms"=> true,
+      "webhook"=> "http://www.egramvyapar.com/dashboard/index",
+      "allow_repeated_payments"=> false
     }
     end
   end
